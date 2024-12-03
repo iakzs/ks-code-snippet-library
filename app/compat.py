2a7ae6dcb2b29dc7e407d958939011aaf9d0e360
iakzs <iakzs@github.com>
2024-12-02 23:01:32 -0300

    I really need to fix

--- app/compat.py
+++
@@ -1,9 +0,0 @@
-"""Compatibility module for handling deprecated Werkzeug functions"""
-import hmac
-
-def safe_str_cmp(a, b):
-    """
-    Replacement for Werkzeug's removed safe_str_cmp function.
-    This is a timing-attack safe comparison function.
-    """
-    return hmac.compare_digest(str(a).encode('utf-8'), str(b).encode('utf-8'))
